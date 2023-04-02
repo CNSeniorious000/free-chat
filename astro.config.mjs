8f39b8161e666afe3da9d66710c0c1497b9b01f1
CNSeniorious000 <wjj123345567@icloud.com>
2023-04-02 19:04:08 +0800

    metadata and styles change

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,9 +35,9 @@ export default defineConfig({
       process.env.OUTPUT !== 'netlify' && VitePWA({
         registerType: 'autoUpdate',
         manifest: {
-          name: 'ChatGPT-API Demo',
-          short_name: 'ChatGPT Demo',
-          description: 'A demo repo based on OpenAI API',
+          name: 'Free Chat - ChatGPT',
+          short_name: 'Free Chat',
+          description: 'Chat with ChatGPT For Free',
           theme_color: '#212129',
           background_color: '#ffffff',
           icons: [
