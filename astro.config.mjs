18d6562aca7992ff9d1100a6ffc3380e9ba4989d
Muspi Merol <wjj123345567@icloud.com>
2023-10-10 21:00:18 +0000

    update everything to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,16 @@ import solidJs from '@astrojs/solid-js'
 import AstroPWA from '@vite-pwa/astro'
 
 import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/edge-functions'
+import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import disableBlocks from './plugins/disableBlocks'
 
 const envAdapter = () => {
   switch (process.env.OUTPUT) {
-    case 'vercel': return vercel()
-    case 'netlify': return netlify()
+    case 'vercel': return vercel({ edgeMiddleware: true })
+    case 'netlify': return netlify({ edgeMiddleware: true })
     default: return node({ mode: 'standalone' })
   }
 }
