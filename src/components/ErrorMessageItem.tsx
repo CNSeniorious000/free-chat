fe7d3525fea42e6ccd1c61961bf153f1da648f83
CNSeniorious000 <wjj123345567@icloud.com>
2023-10-20 04:04:38 +0800

    feat: translate error messages fix: add max width constraint to title

--- src/components/ErrorMessageItem.tsx
+++ src/components/ErrorMessageItem.tsx
@@ -1,3 +1,4 @@
+import { createSignal, onMount } from 'solid-js'
 import IconRefresh from './icons/Refresh'
 import type { ErrorMessage } from '@/types'
 
@@ -7,10 +8,18 @@ interface Props {
 }
 
 export default ({ data, onRetry }: Props) => {
+  const [title, setTitle] = createSignal(data.code)
+  const [description, setDescription] = createSignal(data.message)
+
+  onMount(() => {
+    fetch('/api/translate', { method: 'POST', body: data.code }).then(res => res.text()).then(setTitle)
+    fetch('/api/translate', { method: 'POST', body: data.message }).then(res => res.text()).then(setDescription)
+  })
+
   return (
     <div class="border bg-red/10 border-red/50 my-4 py-3 px-4">
-      {data.code && <div class="text-red mb-1">{data.code}</div>}
-      <div class="text-red text-sm op-70">{data.message}</div>
+      {data.code && <div class="text-red mb-1">{title()}</div>}
+      <div class="text-red text-sm op-70">{description()}</div>
       {onRetry && (
         <div class="mb-2 px-3 fie">
           <div onClick={onRetry} class="text-red ring-red/65 gpt-retry-btn <sm:ring-red/40 hover:bg-red/10">
