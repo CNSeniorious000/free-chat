2788f94806722738675417ce79e2d2171bcbc1d7
Muspi Merol <wjj123345567@icloud.com>
2023-11-09 13:07:06 +0800

    fix: remove `deepl-node` to support edge runtimes

--- src/pages/api/translate.ts
+++ src/pages/api/translate.ts
@@ -1,17 +1,16 @@
-import { Translator } from 'deepl-node'
 import { run } from '../../utils/cf-workers-ai'
-import type { TargetLanguageCode } from 'deepl-node'
 import type { APIRoute } from 'astro'
 
 const deeplAuthKey = import.meta.env.DEEPL_AUTH_TOKEN
 const useDeepL = deeplAuthKey && import.meta.env.TRANSLATE_PROVIDER !== 'cf'
-const target_lang = import.meta.env.TRANSLATE_TARGET_LANG ?? (useDeepL ? 'zh' : 'chinese')
+const target_lang = import.meta.env.TRANSLATE_TARGET_LANG ?? (useDeepL ? 'ZH' : 'chinese')
 
 export const post: APIRoute = async(context) => {
   if (useDeepL) {
-    const translator = new Translator(deeplAuthKey)
-    const result = await translator.translateText(await context.request.text(), null, target_lang as TargetLanguageCode)
-    return new Response(result.text, { headers: { 'x-detected-language': result.detectedSourceLang } })
+    const host = deeplAuthKey.endsWith(':fx') ? 'api-free.deepl.com' : 'api.deepl.com'
+    const headers = { 'Authorization': `DeepL-Auth-Key ${deeplAuthKey}`, 'Content-Type': 'application/json' }
+    const { translations: [{ text, detected_source_language }] } = await fetch(`https://${host}/v2/translate`, { method: 'POST', headers, body: JSON.stringify({ text: [await context.request.text()], target_lang }) }).then(res => res.json())
+    return new Response(text, { headers: { 'x-detected-source-language': detected_source_language } })
   }
   const text = await context.request.text()
   const { result: { translated_text } } = await run('@cf/meta/m2m100-1.2b', { text, target_lang })
