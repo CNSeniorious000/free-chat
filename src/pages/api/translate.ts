9c2745012689bbde183caa19980e59c4d6501dcd
Muspi Merol <wjj123345567@icloud.com>
2023-11-09 12:32:59 +0800

    feat: DeepL as an alternate provider

--- src/pages/api/translate.ts
+++ src/pages/api/translate.ts
@@ -1,9 +1,18 @@
+import { Translator } from 'deepl-node'
 import { run } from '../../utils/cf-workers-ai'
+import type { TargetLanguageCode } from 'deepl-node'
 import type { APIRoute } from 'astro'
 
-const target_lang = import.meta.env.TRANSLATE_TARGET_LANG ?? 'chinese'
+const deeplAuthKey = import.meta.env.DEEPL_AUTH_TOKEN
+const useDeepL = deeplAuthKey && import.meta.env.TRANSLATE_PROVIDER !== 'cf'
+const target_lang = import.meta.env.TRANSLATE_TARGET_LANG ?? (useDeepL ? 'zh' : 'chinese')
 
 export const post: APIRoute = async(context) => {
+  if (useDeepL) {
+    const translator = new Translator(deeplAuthKey)
+    const result = await translator.translateText(await context.request.text(), null, target_lang as TargetLanguageCode)
+    return new Response(result.text, { headers: { 'x-detected-language': result.detectedSourceLang } })
+  }
   const text = await context.request.text()
   const { result: { translated_text } } = await run('@cf/meta/m2m100-1.2b', { text, target_lang })
   return new Response(translated_text)
