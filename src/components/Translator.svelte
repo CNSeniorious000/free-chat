0a0c988e8ee1b7c140ddc235776cffb61e70d65f
Muspi Merol <wjj123345567@icloud.com>
2024-01-02 10:34:46 +0800

    feat: add copy button and refine styles

--- src/components/Translator.svelte
+++ src/components/Translator.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { createParser } from 'eventsource-parser'
   import { promplateBaseUrl } from '@/utils/constants'
+  import UseCopy from './UseCopy.svelte'
 
   const placeholder = 'type or paste your text here'
   let text = ''
@@ -55,9 +56,17 @@
 <div class="m-3 flex flex-col gap-2 md:flex-row">
   <div class="w-full rounded-md bg-white/3 p-2 ring-white/50 transition-all focus-within:(bg-white/7 ring-1.7) lg:p-4 sm:p-3">
     <textarea {placeholder} bind:value={text} class="h-fit min-h-50vh w-full select-none resize-none bg-transparent outline-none md:min-h-80vh placeholder:(text-unset op-30)"></textarea>
-    <div class="flex flex-row self-end justify-end">
-      <button on:click={sync} class="rounded-lg p-2 text-$c-fg ring-1.2 ring-$c-fg ring-inset transition-all not-disabled:(bg-$c-fg text-$c-bg active:scale-95 hover:op-85) disabled:op-40" disabled={!text || loading}>
-        <div class="min-h-4.5 min-w-4.5 flex flex-row items-center justify-center gap-1.5">
+    <div class="flex flex-row self-end justify-end gap-3 [&>button:disabled]:op-40">
+      <UseCopy {text} let:handleClick let:displayText>
+        <button on:click={handleClick} class="not-disabled:(active:scale-95 hover:!ring-white/50 not-focus-visible:!ring-white/10)" disabled={!text}>
+          <div class="min-h-4.5 min-w-4.5 flex flex-row items-center justify-center gap-1">
+            <div i-iconamoon-copy-duotone></div>
+            <span class="text-sm">{displayText}</span>
+          </div>
+        </button>
+      </UseCopy>
+      <button on:click={sync} class="not-disabled:(active:scale-95 not-focus-visible:bg-$c-fg not-focus-visible:text-$c-bg hover:op-85)" disabled={!text || loading}>
+        <div class="min-h-4.5 min-w-4.5 flex flex-row items-center justify-center gap-1">
           {#if loading}
             <div i-svg-spinners-ring-resize text-xs />
             <span class="text-sm">Generating</span>
@@ -70,17 +79,35 @@
     </div>
   </div>
   <div class="w-full rounded-md bg-white/3 p-2 lg:p-4 sm:p-3">
-    {#each parsed as { translated }}
-      {#if translated}
-        <span class="animate-fade-in animate-duration-200"> {translated} </span>
-      {/if}
-    {:else}
-      <span op-30 select-none>translated text will display here</span>
-    {/each}
+    <div class="relative h-full w-full">
+      <div whitespace-pre-line>
+        {#each parsed as { translated }}
+          {#if translated}
+            <span class="animate-fade-in animate-duration-200"> {translated} </span>
+          {/if}
+        {:else}
+          <span op-30 select-none>translated text will display here</span>
+        {/each}
+      </div>
+      <div class="absolute bottom-0 right-0 justify-end gap-3 [&>button:disabled]:op-40">
+        <UseCopy text={parsed.reduce((acc, { translated }) => acc + translated ?? '', '')} let:handleClick let:displayText>
+          <button on:click={handleClick} class="not-disabled:(active:scale-95 hover:!ring-white/50 not-focus-visible:!ring-white/10)" disabled={!parsed.length}>
+            <div class="min-h-4.5 min-w-4.5 flex flex-row items-center justify-center gap-1">
+              <div i-iconamoon-copy-duotone></div>
+              <span class="text-sm">{displayText}</span>
+            </div>
+          </button>
+        </UseCopy>
+      </div>
+    </div>
   </div>
 </div>
 
 <style>
+  button {
+    --uno: rounded-lg py-2 px-2.5 text-$c-fg ring-1.2 ring-$c-fg ring-inset transition-all outline-none;
+  }
+
   ::-webkit-scrollbar {
     --uno: w-1;
   }
---
+++ src/components/UseCopy.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let text: string
+
+  let displayText = 'Copy'
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(text)
+    displayText = 'Copied'
+    setTimeout(() => {
+      displayText = 'Copy'
+    }, 1000)
+  }
+</script>
+
+<slot {handleClick} {displayText} />
