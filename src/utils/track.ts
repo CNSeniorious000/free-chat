88d79598a0b25d12035b92da4d37a7c58db79dd5
Muspi Merol <wjj123345567@icloud.com>
2024-01-15 06:26:22 +0800

    fix: cannot read properties of undefined

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,6 +1,6 @@
 export function trackEvent(event: string, data?: Record<string, any>) {
   // @ts-expect-error umami is optional
-  window?.umami.track(event, data)
+  window.umami && window.umami?.track(event, data)
   // @ts-expect-error gtag is optional
-  window?.gtag('event', event, data)
+  window.gtag && window.gtag('event', event, data)
 }
