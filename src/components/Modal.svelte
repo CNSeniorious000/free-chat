1f3a9f7faa364874173e7b49a3fed0a0b2f67ff1
Muspi Merol <wjj123345567@icloud.com>
2024-09-29 21:08:04 +0800

    feat: apply backdrop overlay on `theme-color` too

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
   import { ripple } from 'svelte-ripple-action'
+  import { setThemeColor } from './ThemeColor.svelte'
   export let show = true
   export let icon: string
+
+  let timeoutId: ReturnType<typeof setTimeout>
+  let transitioning = false
+
+  function darkenThemeColor(show: boolean) {
+    if (typeof window === 'undefined') return
+
+    clearTimeout(timeoutId)
+
+    let color: string
+
+    if (!show) {
+      color = 'var(--c-bg)'
+    } else {
+      const isDark = document.documentElement.classList.contains('dark')
+      color = isDark ? '#141419' : '#e1e1e1'
+    }
+
+    clearTimeout(timeoutId)
+
+    if (transitioning)
+      setThemeColor(color) // immediately
+    else
+      timeoutId = setTimeout(() => setThemeColor(color), 200)
+  }
+
+  $: darkenThemeColor(show)
 </script>
 
-<div role="none" on:click={() => (show = false)} class="fixed inset-0 z-100 bg-black/10 transition-opacity duration-800 dark:bg-black/40" class:pointer-events-none={!show} class:op-0={!show} />
+<div role="none" on:click={() => (show = false)} class="fixed inset-0 z-100 bg-black/10 transition-opacity duration-800 dark:bg-black/40" class:pointer-events-none={!show} class:op-0={!show} on:transitionstart={() => { transitioning = true }} on:transitionend={() => { transitioning = false }} />
 <section class="pointer-events-none fixed bottom-0 left-0 right-0 z-1000 flex flex-row select-none justify-center tracking-wide transition duration-400" class:[&_*]:pointer-events-auto={show} class:op-0={!show} class:translate-y-10={!show} class:ease-out={show} class:ease-in={!show}>
   <div class="relative m-5vmin w-xl flex flex-col gap-3 rounded-lg bg-$c-bg p-5 shadow-xl [&_a]:(mx-1 border-b-1.5 border-$c-fg-20 pb-0.3 transition-border-color dark:border-$c-fg) [&_a:hover]:(border-$c-fg-60 dark:border-$c-fg-40)">
     <div class="relative z-1 flex flex-row items-center gap-3 text-4xl">
