61a1d37ffec55ec17b68f86966f0c08e3c2e3d27
Muspi Merol <wjj123345567@icloud.com>
2024-10-02 23:02:23 +0800

    feat: implement sticky header

---
+++ src/components/Inview.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { inview } from 'svelte-inview'
+
+  export let inView: boolean
+</script>
+
+<div use:inview on:inview_change={({ detail }) => { inView = detail.inView }} {...$$restProps} role="presentation" />
