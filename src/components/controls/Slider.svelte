07b6ba7f4c64a5844b5e76c7e5d5ebac12bf5959
Muspi Merol <me@promplate.dev>
2025-02-16 12:12:47 +0800

    fix: update Slider component

--- src/components/controls/Slider.svelte
+++ src/components/controls/Slider.svelte
@@ -15,16 +15,16 @@
   <div class="i-ci-redo"></div>
 </button>
 
-<Slider.Root let:thumbs min={0} max={1} step={0.05} class="relative mx-1 h-full w-full" value={[$value ?? 1]} onValueChange={([v]) => ($value = v)} >
+<Slider.Root type="single" min={0} max={1} step={0.05} class="relative mx-1 h-full w-full" value={$value ?? 1} onValueChange={value.set}>
   <div class="absolute top-1/2 h-0.8 rounded-full bg-$c-fg-10 -left-1 -right-1 -translate-y-1/2"></div>
   {#if $value !== undefined}
     <Slider.Range class="top-1/2 h-0.8 rounded-full bg-$c-fg-20 -translate-y-1/2 !-left-1" />
   {/if}
-  {#each thumbs as thumb}
-  <Slider.Thumb {thumb} asChild let:builder>
-    <div class:hidden={$value === undefined} class="relative top-1/2 h-fit rounded-full bg-#f7f7f7 outline-none -translate-y-1/2 dark:bg-#24242d focus-visible:bg-$c-fg-20" {...builder} use:builder.action>
-      <div class="m-0.9 h-3.2 w-0.9 rounded-full bg-$c-fg-80"></div>
-    </div>
+  <Slider.Thumb index={0}>
+    {#snippet child({ props })}
+      <div class:hidden={$value === undefined} class="relative top-1/2 h-fit rounded-full bg-#f7f7f7 outline-none -translate-y-1/2 dark:bg-#24242d focus-visible:bg-$c-fg-20" {...props}>
+        <div class="m-0.9 h-3.2 w-0.9 rounded-full bg-$c-fg-80"></div>
+      </div>
+    {/snippet}
   </Slider.Thumb>
-  {/each}
 </Slider.Root>
