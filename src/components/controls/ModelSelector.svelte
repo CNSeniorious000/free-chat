d94be7dee3b2f2f49c1392261f0d652eb49adad5
Muspi Merol <me@promplate.dev>
2025-03-23 00:59:54 +0800

    feat: add gemini and gemma 3 llms

--- src/components/controls/ModelSelector.svelte
+++ src/components/controls/ModelSelector.svelte
@@ -10,8 +10,10 @@
   <ModelItem id="deepseek-ai/DeepSeek-V3" name="deepseek-v3" title="✨ 深度求索最新发布的 DeepSeek v3" />
   <ModelItem id="llama-3.3-70b" name="llama3.3-70b" title="🚀 Meta 最新发布的 Llama3.3 70b" />
   <ModelItem id="llama-3.2-90b-vision-preview" name="llama3.2-90b" title="🚀 Meta 最新发布的 Llama3.2 90b" />
-  <ModelItem id="gpt-4o-mini" name="gpt-4o-mini" title="🔥 OpenAI 最新的全能模型" />
   <ModelItem id="grok-2-1212" name="grok-2-1212" title="🚀 xAI 的第二代 Grok" />
+  <ModelItem id="gemma-3-27b-it" name="gemma 3" title="Google 最新的开源模型" />
+  <ModelItem id="gemini-2.0-flash-lite" name="gemini 2.0 flash lite" title="Google 最新一代 Gemini" />
+  <ModelItem id="gemini-2.0-flash-thinking-exp" name="gemini 2.0 flash thinking" title="Google 的推理模型" />
   <!-- <ModelItem id="internlm/internlm2_5-20b-chat" name="internlm2.5-20b" title="✨ 书生·浦语" /> -->
   <ModelItem id="yi-lightning" name="yi-lightning" title="⭐ 零一万物的霸榜 SOTA 模型" />
   <ModelItem id="THUDM/glm-4-9b-chat" name="glm-4-9b-chat" title="✨ 智谱 AI" />
@@ -22,6 +24,6 @@
   <ModelItem id="Mistral-large-2411" name="Mistral-large" title="Mistral 旗舰款" />
   <!-- <ModelItem id="Phi-3.5-MoE-instruct" name="Phi-3.5-MoE-instruct" title="微软的开源模型" /> -->
   <!-- <ModelItem id="Meta-Llama-3.1-405B-Instruct" name="llama-3.1-405b" title="Llama-3.1 405B 超大杯" /> -->
-  <ModelItem id="AI21-Jamba-1.5-Large" name="Jamba-1.5-Large" title="AI21 Jamba 1.5 Large" />
-  <ModelItem id="AI21-Jamba-1.5-Mini" name="Jamba-1.5-Mini" title="AI21 Jamba 1.5 Mini" />
+  <ModelItem id="azure:gpt-4o-mini" name="gpt-4o-mini" title="🔥 OpenAI 最新的全能模型" />
+  <ModelItem id="Phi-4" name="Phi 4" title="微软 Phi 系列最新款" />
 </div>
