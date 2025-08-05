46c17bc8f7d0c32b79e6422a8ea9ce270fe5e5e6
Muspi Merol <me@promplate.dev>
2025-08-06 03:07:33 +0800

    Implement lazy-loading for the `katex` package

--- src/components/markdown/LaTeX.tsx
+++ src/components/markdown/LaTeX.tsx
@@ -1,7 +1,12 @@
-import { render } from 'katex'
+import type { KatexOptions } from 'katex'
+
 import { createEffect, createSignal } from 'solid-js'
 
 export default function LaTeX(props: { value: string, inline: boolean }) {
+  async function render(value: string, element: HTMLElement, options?: KatexOptions) {
+    const { render } = await import('katex')
+    render(value, element, options)
+  }
   const [ref, setRef] = createSignal<HTMLDivElement>()
   createEffect(() => {
     ref() && render(props.value, ref()!, { throwOnError: false, displayMode: !props.inline, trust: true })
