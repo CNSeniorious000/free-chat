00cd0b7637f3c6782461a17b13ce68c411e77c29
Muspi Merol <me@promplate.dev>
2025-08-06 04:58:55 +0800

    feat: add `gpt-oss-120b` and `qwen-3-coder-480b`

--- src/components/controls/ModelSelector.svelte
+++ src/components/controls/ModelSelector.svelte
@@ -8,6 +8,8 @@
   <ModelItem id="moonshotai/kimi-k2:free" name="kimi-k2" title="🌚 月之暗面 Kimi K2" />
   <ModelItem id="deepseek/deepseek-r1-0528:free" name="deepseek-r1-0528" title="🐋 DeepSeek 最新版 R1" />
   <ModelItem id="deepseek-r1-distill-llama-70b" name="deepseek-r1-llama-70b" title="🧠 DeepSeek 的推理模型 R1" />
+  <ModelItem id="gpt-oss-120b" name="gpt-oss-120b" title="🚀 OpenAI 最新开源的推理模型" />
+  <ModelItem id="qwen-3-coder-480b" name="qwen-3-coder-480b" title="🧠 旗舰代码模型 Qwen 3 480B" />
   <ModelItem id="qwen-3-235b-a22b-thinking-2507" name="qwen-3-235b-thinking" title="🧠 旗舰混合推理模型 Qwen 3 235B" />
   <ModelItem id="qwen-3-235b-a22b-instruct-2507" name="qwen-3-235b-instruct" title="✨ 通义千问最新模型 Qwen 3 235B" />
   <ModelItem id="meta-llama/llama-4-maverick-17b-128e-instruct" name="llama4-maverick" title="🚀 Meta 最新的 Llama 4 Maverick" />
