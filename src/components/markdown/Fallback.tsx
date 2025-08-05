dea6134a7ae5908a1637fea5403753420553b150
Muspi Merol <me@promplate.dev>
2025-08-06 02:39:43 +0800

    Support rendering LaTeX blocks

--- src/components/markdown/Fallback.tsx
+++ src/components/markdown/Fallback.tsx
@@ -1,20 +1,32 @@
-import type { Node, Parent, Root, Text } from 'mdast'
+import type { Code, Node, Parent, Root, Text } from 'mdast'
 
 import { toHtml } from 'hast-util-to-html'
 import { toHast } from 'mdast-util-to-hast'
-import { createMemo, Show } from 'solid-js'
+import { createMemo, Match, Switch } from 'solid-js'
 
 import LaTeX from './LaTeX'
 
 const isLaTeX = (node: ReturnType<typeof toHast>) => {
-  return node.type === 'element' && node.tagName === 'code' && (node.properties?.className as string[])?.includes('math-inline')
+  if (node.type === 'element') {
+    if (node.tagName === 'code' && (node.properties?.className as string[])?.includes('language-math')) {
+      return 'inline'
+    } else if (node.tagName === 'pre' && isLaTeX(node.children[0])) {
+      return 'block'
+    }
+  }
+  return false
 }
 
 export default function Fallback(props: { node: Node }) {
   const hast = createMemo(() => toHast(props.node as Root))
   return (
-    <Show when={isLaTeX(hast())} fallback={<div class="contents" innerHTML={toHtml(hast())}></div>}>
-      <LaTeX value={((hast() as Parent).children as [Text])[0].value} inline={true} />
-    </Show>
+    <Switch fallback={<div class="contents" innerHTML={toHtml(hast())}></div>}>
+      <Match when={isLaTeX(hast()) === 'inline'}>
+        <LaTeX value={((hast() as Parent).children as [Code])[0].value} inline={true} />
+      </Match>
+      <Match when={isLaTeX(hast()) === 'block'}>
+        <LaTeX value={(((hast() as Parent).children[0] as Parent).children as [Text])[0].value} inline={false} />
+      </Match>
+    </Switch>
   )
 }
--- src/components/markdown/LaTeX.tsx
+++ src/components/markdown/LaTeX.tsx
@@ -4,7 +4,7 @@ import { createEffect, createSignal } from 'solid-js'
 export default function LaTeX(props: { value: string, inline: boolean }) {
   const [ref, setRef] = createSignal<HTMLDivElement>()
   createEffect(() => {
-    ref() && render(props.value, ref()!, { throwOnError: false, displayMode: !props.inline })
+    ref() && render(props.value, ref()!, { throwOnError: false, displayMode: !props.inline, trust: true })
   })
-  return <div ref={setRef} />
+  return <div ref={setRef} class="contents" />
 }
