0dc36cf71a33b4f3e1eca5d26d4796113544c371
Muspi Merol <wjj123345567@icloud.com>
2025-12-31 20:25:45 +0800

    chore: remove unusable models and add available ones

--- src/components/controls/ModelSelector.svelte
+++ src/components/controls/ModelSelector.svelte
@@ -26,9 +26,9 @@
   <!-- <ModelItem id="internlm/internlm2_5-20b-chat" name="internlm2.5-20b" title="✨ 书生·浦语" /> -->
   <ModelItem id="yi-lightning" name="yi-lightning" title="零一万物" />
   <ModelItem id="kwaipilot/kat-coder-pro:free" name="Kat Coder Pro" title="✨ 快手出品的编程模型" />
-  <ModelItem id="allenai/olmo-3-32b-think:free" name="olmo-3-32b-think" title="🧠 开源的深度推理模型" />
-  <ModelItem id="amazon/nova-2-lite-v1:free" name="nova-2-lite-v1" title="适用于日常任务 / 长上下文" />
+  <ModelItem id="allenai/olmo-3.1-32b-think:free" name="olmo-3.1-32b-think" title="🧠 开源的深度推理模型" />
   <ModelItem id="arcee-ai/trinity-mini:free" name="trinity-mini" title="arcee-ai 高效的开源模型" />
+  <ModelItem id="nvidia/nemotron-3-nano-30b-a3b:free" name="nemotron-3-nano" title="NVIDIA 的开源模型" />
   <Hr text="低可用性模型（延迟较大）" />
   <!-- <ModelItem id="azure:gpt-4o" name="gpt-4o" title="GPT 4o (Azure)" /> -->
   <!-- <ModelItem id="azure:gpt-4o-mini" name="gpt-4o-mini" title="GPT 4o mini (Azure)" /> -->
