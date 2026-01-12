0def40c337de5e6339a40f9ca129e261ede316e4
Muspi Merol <me@promplate.dev>
2026-01-13 00:37:14 +0800

    chore: update model list

--- src/components/controls/ModelSelector.svelte
+++ src/components/controls/ModelSelector.svelte
@@ -12,7 +12,7 @@
   <ModelItem id="z-ai/glm-4.5-air:free" name="glm-4.5-air" title="✨ 智谱清言开源巨作 GLM 4.5" />
   <ModelItem id="moonshotai/kimi-k2-instruct-0905" name="kimi-k2" title="🌚 月之暗面 Kimi K2" />
   <ModelItem id="tngtech/tng-r1t-chimera:free" name="tng-r1t-chimera" title="💞 创意故事与角色互动实验模型 (较慢)" />
-  <ModelItem id="zai-glm-4.6" name="glm-4.6" title="🚀 智谱清言开源巨作 GLM 4.6" />
+  <ModelItem id="zai-glm-4.7" name="glm-4.7" title="🚀 智谱清言开源巨作 GLM 4.7" />
   <ModelItem id="gpt-oss-120b" name="gpt-oss-120b" title="🚀 OpenAI 最新开源的推理模型" />
   <ModelItem id="qwen-3-32b" name="qwen-3-32b" title="🧠 Qwen 通义千问推理模型 Qwen 3 32B" />
   <ModelItem id="qwen-3-235b-a22b-instruct-2507" name="qwen-3-235b-instruct" title="✨ 通义千问最新模型 Qwen 3 235B" />
@@ -22,13 +22,9 @@
   <ModelItem id="deepseek-ai/DeepSeek-V3.2-Exp" name="deepseek-v3.2-exp" title="🐋 DeepSeek 最新款" />
   <ModelItem id="gemma-3-27b-it" name="gemma 3" title="Google 最新的开源模型" />
   <ModelItem id="gemini-2.5-flash-lite" name="gemini 2.5 flash lite" title="Google 最新一代 Gemini" />
-  <ModelItem id="nex-agi/deepseek-v3.1-nex-n1:free" name="deepseek-v3.1-nex-n1" title="DeepSeek 上海创智学院后训练版" />
   <!-- <ModelItem id="internlm/internlm2_5-20b-chat" name="internlm2.5-20b" title="✨ 书生·浦语" /> -->
   <ModelItem id="yi-lightning" name="yi-lightning" title="零一万物" />
-  <ModelItem id="kwaipilot/kat-coder-pro:free" name="Kat Coder Pro" title="✨ 快手出品的编程模型" />
-  <ModelItem id="allenai/olmo-3.1-32b-think:free" name="olmo-3.1-32b-think" title="🧠 开源的深度推理模型" />
   <ModelItem id="arcee-ai/trinity-mini:free" name="trinity-mini" title="arcee-ai 高效的开源模型" />
-  <ModelItem id="nvidia/nemotron-3-nano-30b-a3b:free" name="nemotron-3-nano" title="NVIDIA 的开源模型" />
   <Hr text="低可用性模型（延迟较大）" />
   <!-- <ModelItem id="azure:gpt-4o" name="gpt-4o" title="GPT 4o (Azure)" /> -->
   <!-- <ModelItem id="azure:gpt-4o-mini" name="gpt-4o-mini" title="GPT 4o mini (Azure)" /> -->
